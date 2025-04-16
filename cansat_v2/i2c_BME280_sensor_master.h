e4624d6bb01f899e0f47ef8f126186a82e54fb4e
Marie Louise Quitoriano <mariequitoriano24@gmail.com>
2025-04-16 22:14:11 +0800

    Create i2c_BME280_sensor_master.h

---
+++ cansat_v2/i2c_BME280_sensor_master.h
@@ -0,0 +1,14 @@
+// i2c master header file 
+// purpose of this portion of the code:
+// this is supposed to serve as a master file to communicate with the sensor
+// so the master is the pic32, and the slave is the BME280 sensor
+
+#include <xc.h>
+typedef enum{
+    ACK_THEN_STOP, //ack the stuff then stop
+    ACK_THEN_REPEATED_START//repeated start
+}StopCondition;//stop condition here
+
+uint16_t SERCOM2_I2C_Write_Polled(uint8_t addr, uint8_t *buf, uint16_t len);
+uint16_t SERCOM2_I2C_Read_Polled(uint8t addr, uint8_t *buf, uint16_t len);
+void SERCOM2_I2C_Initialize(void); // write the c file for this i2c master
