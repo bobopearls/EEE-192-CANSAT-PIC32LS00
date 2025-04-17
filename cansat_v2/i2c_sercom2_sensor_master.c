0b06de8c6ed49d27fc6dba04ecaeb9903431bc25
Marie Louise Quitoriano <mariequitoriano24@gmail.com>
2025-04-18 00:17:32 +0800

    Update i2c_sercom2_sensor_master.c

--- cansat_v2/i2c_sercom2_sensor_master.c
+++ cansat_v2/i2c_sercom2_sensor_master.c
@@ -4,7 +4,7 @@
 // then the sercom2 clk init, init, set baud rates and then the pins we will use, etc.
 
 #include <xc.h>
-#include "i2c_BME280_sensor_master.h"
+#include "i2c_sercom2_sensor_master.h"
 
 void SERCOM2_I2C_Clock_Init(){
     // the APB C periperal clocks are already enabled by default so we dont need to enable it manually
@@ -30,7 +30,8 @@ void SERCOM2_I2C_Init(void){
     }
     
     //setting the baud rate for the master (bme280 can handle the standard and fast mode, 100kHz and 400kHz)
-    SERCOM2_REGS->I2CM.SERCOM_BAUD = 0x00;
+    SERCOM2_REGS->I2CM.SERCOM_BAUD = 0x04; // this is for the 400 kHz fast mode if we want the slow mode, use 0x13;
+    // original sir berts code uses 0xE8 so check that later.
     
     // set up the i2c interrupts such as the data transfer completion and the error handling 
     SERCOM2_REGS->I2CM.SERCOM_INTENSET = SERCOM_I2CM_INTENSET_MB_Msk | 
@@ -210,4 +211,100 @@ uint16_t SERCOM2_I2C_Write_Polled_Init(uint8_t addr, uint8_t *buf, uint16_t len,
         }
     }
     return pos;
+}
+
+uint16_t SERCOM2_I2C_Read_Polled_Init(uint8_t addr, uint8_t *buf, uint16_t len, StopCondition S_condt){
+    uint8_t flags = 0;
+    uint16_t pos = 0;
+    uint32_t temp = 0;
+    
+    #define I2C_STATUS_MASK 0x3
+    #define I2C_STATE_IDLE 0x1UL
+    
+    /******** Step 0: Wait for the bus to become idle ********/
+    switch (S_condt){
+        case ACK_THEN_STOP:
+            // poll until we reach idle state
+            while(((SERCOM2_REGS->I2CM.SERCOM_STATUS >> 4) & I2C_STATUS_MASK)!= I2C_STATE_IDLE){
+                asm("nop");
+            }
+            break; // for repeated start the machine just stays busy and continues
+    }
+    
+    SERCOM2_REGS->I2CM.SERCOM_CTRLB = SERCOM_I2CM_CTRLB_SMEN_Msk; // enable the smart mode
+    /* Wait for synchronization */
+    while((SERCOM2_REGS->I2CM.SERCOM_SYNCBUSY) != 0U);
+    
+    // Step 1: Initialize transfer by writing to ADDR (ADDR[0] indicates direction)
+    SERCOM2_REGS->I2CM.SERCOM_ADDR = ((addr << 1) | 0x01); // Read operation (LSB = 1)
+    while(SERCOM2_REGS->I2CM.SERCOM_SYNCBUSY & SERCOM_I2CM_SYNCBUSY_SYSOP_Msk);
+    // wait and process interrupt flags
+    while((flags = (SERCOM2_REGS->I2CM.SERCOM_INTFLAG &
+                   (SERCOM_I2CM_INTFLAG_MB_Msk |
+                    SERCOM_I2CM_INTFLAG_ERROR_Msk))) == 0){
+        if(++temp > 10000){
+            SERCOM2_I2C_ABORT();
+            return 0;
+        }
+    }
+    // check for an error right after addr, if error, abort and exit
+    if (flags & SERCOM_I2CM_INTFLAG_ERROR_Msk || 
+                SERCOM2_REGS->I2CM.SERCOM_STATUS & 
+               (SERCOM_I2CM_STATUS_RXNACK_Msk | SERCOM_I2CM_STATUS_ARBLOST_Msk)){
+        SERCOM2_I2C_ABORT();
+        return 0;
+    }
+    
+    // 3: read data then respond w/acks
+    for (pos; pos < len - 1; pos++){
+        while ((SERCOM2_REGS->I2CM.SERCOM_INTFLAG & SERCOM_I2CM_INTFLAG_SB_Msk) != SERCOM_I2CM_INTFLAG_SB(0x1)) asm("nop"); // wait for data
+        buf[pos] = (uint8_t) SERCOM2_REGS->I2CM.SERCOM_DATA; // read the data
+        
+        // if error, abort and exit
+        // then check for ACK or NCK errors
+        if((SERCOM2_REGS->I2CM.SERCOM_STATUS & SERCOM_I2CM_STATUS_RXNACK_Msk) || // no ack
+          (SERCOM2_REGS->I2CM.SERCOM_STATUS & SERCOM_I2CM_STATUS_ARBLOST_Msk)){ // collision detected
+            SERCOM2_I2C_ABORT();
+            return 0; // do this if error occurs
+        }
+    }
+    
+    // 4: stop condition then return state to idle
+    while ((SERCOM2_REGS->I2CM.SERCOM_INTFLAG & SERCOM_I2CM_INTFLAG_SB_Msk) != SERCOM_I2CM_INTFLAG_SB(0x1)) asm("nop");
+    
+    /* 
+     * SET CTRLB.ACKACT = 0x1 to set ACKACT = NACK 
+     * SET CTRLB.CMD    = 0x3 to send ACKACT (NACK), then trigger stop condition
+     */
+    SERCOM2_REGS->I2CM.SERCOM_CTRLB |= SERCOM_I2CM_CTRLB_ACKACT_Msk | SERCOM_I2CM_CTRLB_CMD(3UL);
+    // SYNCBUSY.SYSOP is busy when you write to SERCOM_CTRLB
+    while (SERCOM2_REGS->I2CM.SERCOM_SYNCBUSY != 0U) asm("nop");
+    
+    /* Read DATA.DATA after we set CTRLB to send a NACK and STOP condition. */
+    buf[pos] = (uint8_t) SERCOM2_REGS->I2CM.SERCOM_DATA;
+    
+    /* Wait until internal state machine is at the idle state again */
+    while((SERCOM2_REGS->I2CM.SERCOM_STATUS & SERCOM_I2CM_STATUS_BUSSTATE_Msk) != SERCOM_I2CM_STATUS_BUSSTATE(0x01U));
+    
+    return pos;
+}
+
+uint16_t SERCOM2_I2C_Write_Polled(uint8_t addr, uint8_t *buf, uint16_t len) {
+    /* We do this because we need a repeated Start for ReadReg */
+    StopCondition S_condt = ACK_THEN_STOP;
+    SERCOM2_I2C_Write_Polled_Init(addr, buf, len, S_condt);
+}
+
+uint16_t SERCOM2_I2C_Read_Polled(uint8_t addr, uint8_t *buf, uint16_t len) {
+    /* We do this because we need a repeated Start for ReadReg */
+    StopCondition S_condt = ACK_THEN_STOP;
+    SERCOM2_I2C_Read_Polled_Init(addr, buf, len, S_condt);
+}
+
+uint8_t SERCOM2_I2C_ReadReg(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *buf, uint8_t read_len) {
+    uint8_t tmp[1] = {reg_addr};
+    StopCondition S_condt = ACK_THEN_REPEATED_START;
+    SERCOM2_I2C_Write_Polled_Init(i2c_addr, tmp, 1, S_condt);
+    SERCOM2_I2C_Read_Polled_Init(i2c_addr, buf, read_len, S_condt);
+    return 0;
 }
